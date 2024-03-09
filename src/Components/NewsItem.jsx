2abb90582694701e3772a4a6a3fe5c9fff1c259a
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-09 21:20:27 +0530

    done

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,16 +1,17 @@
+import image from '../assets/news-image.jpg'
 
 function NewsItem({title, description, src, url}) {
   return (
-    <div>
-      <div className="card bg-dark text-light mb-3 d-inline-block mg-3 mx-3 px-2" style={{maxWidth:"345px"}}>
-      <img src={src} style={{height:"200px",width:"360px"}} className="card-img-top" alt="..."/>
+    
+      <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
+      <img src={src?src:image} style={{height:"200px",width:"325px"}} className="card-img-top" alt="..."/>
       <div className="card-body">
-         <h5 className="card-title">{title}</h5>
-         <p className="card-text">{description}</p>
+         <h5 className="card-title">{title.slice(0,50)}</h5>
+         <p className="card-text">{description?description.slice(0,90):"Custom Description added"}</p>
          <a href={url} className="btn btn-primary">Read more</a>
       </div>
     </div>
-    </div>
+    
   )
 }
 
