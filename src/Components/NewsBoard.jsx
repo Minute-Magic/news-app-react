07f223ad76e44d5d2192f3885d02ff531396f3cb
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-09 18:57:05 +0530

    map articles

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import NewsItem from "./NewsItem";
 
 
 function NewsBoard() {
+  const [articles,SetArticles] = useState([]);
+  useEffect(()=>{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url).then(response => response.json()).then(data => SetArticles(data.articles));
+  },[])
   return (
     <div>
       <h2 className="text-center">Latest<span className="badge bg-danger">News</span></h2>
+      {articles.map((news,index)=>{
+        return< NewsItem key={index} title={news.title} description={news.description} src = {news.urlToImage} url = {news.url}/>
+      })}
     </div>
   )
 }
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,7 +1,16 @@
 
-function NewsItem() {
+function NewsItem({title, description, src, url}) {
   return (
-    <div>NewsItem</div>
+    <div>
+      <div className="card" style={{maxWidth:"345px"}}>
+      <img src={src} className="card-img-top" alt="..."/>
+      <div className="card-body">
+         <h5 className="card-title">{title}</h5>
+         <p className="card-text">{description}</p>
+         <a href={url} className="btn btn-primary">Read more</a>
+      </div>
+    </div>
+    </div>
   )
 }
 
